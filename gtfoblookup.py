6ca6835aaf89a5b4d2d56d724569cdb62bd80731
James conlan <james.conlan@nccgroup.com>
2019-08-22 14:44:20 +0100

    Tidied up to reduce duplicate code

--- gtfoblookup.py
+++ gtfoblookup.py
@@ -41,6 +41,7 @@ def parseArgs():
     """Parses command line arguments"""
     parser = argparse.ArgumentParser(description='Offline GTFOBins Lookup')
     subparsers = parser.add_subparsers()
+    typParsers = []
     #Update
     parserUpdate = subparsers.add_parser('update', help="update local copy " + 
                                          "of GTFOBins")
@@ -53,125 +54,83 @@ def parseArgs():
     parserShell = subparsers.add_parser('shell', help="search for " + 
                                         "applications that can be used to " + 
                                         "spawn an interactive shell")
-    parserShell.set_defaults(func=search, typ=types['shell'])
-    parserShell.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserShell.add_argument('binary', help='the binary to search for')
+    parserShell.set_defaults(typ=types['shell'])
+    typParsers.append("parserShell")
     #Command
     parserCmd = subparsers.add_parser('cmd', help="search for applications " + 
                                       "that can be used to run " + 
                                       "non-interactive system commands")
-    parserCmd.set_defaults(func=search, typ=types['cmd'])
-    parserCmd.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserCmd.add_argument('binary', help='the binary to search for')
+    parserCmd.set_defaults(typ=types['cmd'])
+    typParsers.append("parserCmd")
     #Reverse shell
     parserRev = subparsers.add_parser('rev', help="search for applications " + 
                                       "that can be used to spawn a reverse " + 
                                       "shell")
-    parserRev.set_defaults(func=search, typ=types['rev'])
-    parserRev.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserRev.add_argument('binary', help='the binary to search for')
+    parserRev.set_defaults(typ=types['rev'])
+    typParsers.append("parserRev")
     #Non-interactive reverse shell
     parserNrev = subparsers.add_parser('nrev', help="search for applications " + 
                                        "that can be used to spawn a " + 
                                        "non-interactive reverse shell")
-    parserNrev.set_defaults(func=search, typ=types['nrev'])
-    parserNrev.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserNrev.add_argument('binary', help='the binary to search for')
+    parserNrev.set_defaults(typ=types['nrev'])
+    typParsers.append("parserNrev")
     #Bind shell
     parserBind = subparsers.add_parser('bind', help="search for applications " + 
                                        "that can be used to spawn a bind shell")
-    parserBind.set_defaults(func=search, typ=types['bind'])
-    parserBind.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserBind.add_argument('binary', help='the binary to search for')
+    parserBind.set_defaults(typ=types['bind'])
+    typParsers.append("parserBind")
     #Non-interactive bind shell
     parserNbind = subparsers.add_parser('nbind', help="search for " + 
                                         "applications that can be used to " + 
                                         "spawn a non-interactive bind shell")
-    parserNbind.set_defaults(func=search, typ=types['nbind'])
-    parserNbind.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserNbind.add_argument('binary', help='the binary to search for')
+    parserNbind.set_defaults(typ=types['nbind'])
+    typParsers.append("parserNbind")
     #File upload
     parserUpload = subparsers.add_parser('upload', help="search for " + 
                                          "applications that can be used to " + 
                                          "upload files")
-    parserUpload.set_defaults(func=search, typ=types['upload'])
-    parserUpload.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserUpload.add_argument('binary', help='the binary to search for')
+    parserUpload.set_defaults(typ=types['upload'])
+    typParsers.append("parserUpload")
     #File download
     parserDownload = subparsers.add_parser('download', help="search for " + 
                                            "applications that can be used to " +
                                            "download files")
-    parserDownload.set_defaults(func=search, typ=types['download'])
-    parserDownload.add_argument('-f', '--file', help="use a file containing " +
-                  "a list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserDownload.add_argument('binary', help='the binary to search for')
+    parserDownload.set_defaults(typ=types['download'])
+    typParsers.append("parserDownload")
     #File write
     parserWrite = subparsers.add_parser('write', help="search for " + 
                                         "applications that can be used to " + 
                                         "write to files")
-    parserWrite.set_defaults(func=search, typ=types['write'])
-    parserWrite.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserWrite.add_argument('binary', help='the binary to search for')
+    parserWrite.set_defaults(typ=types['write'])
+    typParsers.append("parserWrite")
     #File read
     parserRead = subparsers.add_parser('read', help="search for applications " +
                                        "that can be used to read files")
-    parserRead.set_defaults(func=search, typ=types['read'])
-    parserRead.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserRead.add_argument('binary', help='the binary to search for')
+    parserRead.set_defaults(typ=types['read'])
+    typParsers.append("parserRead")
     #Library load
     parserLoad = subparsers.add_parser('load', help="search for applications " +
                                        "that load shared libraries")
-    parserLoad.set_defaults(func=search, typ=types['load'])
-    parserLoad.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserLoad.add_argument('binary', help='the binary to search for')
+    parserLoad.set_defaults(typ=types['load'])
+    typParsers.append("parserLoad")
     #SUID
     parserSuid = subparsers.add_parser('suid', help="search for applications " +
                                        "that, with the SUID bit set, can be " + 
                                        "used to escalate privileges")
-    parserSuid.set_defaults(func=search, typ=types['suid'])
-    parserSuid.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserSuid.add_argument('binary', help='the binary to search for')
+    parserSuid.set_defaults(typ=types['suid'])
+    typParsers.append("parserSuid")
     #Sudo
     parserSudo = subparsers.add_parser('sudo', help="search for applications " + 
                                        "that, when run with sudo, can be used" + 
                                        " to escalate privileges")
-    parserSudo.set_defaults(func=search, typ=types['sudo'])
-    parserSudo.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserSudo.add_argument('binary', help='the binary to search for')
+    parserSudo.set_defaults(typ=types['sudo'])
+    typParsers.append("parserSudo")
     #Capabilities
     parserCap = subparsers.add_parser('cap', help="search for applications " + 
                                       "that have the 'CAP_SETUID' capability " +
                                       "set")
-    parserCap.set_defaults(func=search, typ=types['cap'])
-    parserCap.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserCap.add_argument('binary', help='the binary to search for')
+    parserCap.set_defaults(typ=types['cap'])
+    typParsers.append("parserCap")
     #Limited SUID
     parserLsuid = subparsers.add_parser('lsuid', help="search for " + 
                                         "applications that, with the SUID " + 
@@ -179,19 +138,21 @@ def parseArgs():
                                         "privileges on systems that allow " + 
                                         "the default 'sh' shell to run with " + 
                                         "sudo privileges")
-    parserLsuid.set_defaults(func=search, typ=types['lsuid'])
-    parserLsuid.add_argument('-f', '--file', help="use a file containing a " +
-                  "list of binaries \(one per line\) instead of a single " +
-                  "binary", action='store_const', const=parseFile, dest='func')
-    parserLsuid.add_argument('binary', help='the binary to search for')
+    parserLsuid.set_defaults(typ=types['lsuid'])
+    typParsers.append("parserLsuid")
     #All
     parserAll = subparsers.add_parser('all', help="search for applications " +
                                       "in all categories")
-    parserAll.set_defaults(func=search, typ="all")
-    parserAll.add_argument('-f', '--file', help="use a file containing a " +
+    parserAll.set_defaults(typ="all")
+    typParsers.append("parserAll")
+    #Set common properties
+    for subparser in typParsers:
+        subparser = vars()[subparser]
+        subparser.set_defaults(func=search)
+        subparser.add_argument('-f', '--file', help="use a file containing a " +
                   "list of binaries \(one per line\) instead of a single " +
                   "binary", action='store_const', const=parseFile, dest='func')
-    parserAll.add_argument('binary', help='the binary to search for')
+        subparser.add_argument('binary', help='the binary to search for')
     #No args
     if len(sys.argv) == 1:
         parser.print_usage()
